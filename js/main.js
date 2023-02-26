47379100920989d7b52228e6060b28537e9c6a8a
BganaRoxa <89401543+BganaRoxa@users.noreply.github.com>
2023-02-26 18:30:58 -0400

    remodelagem do site
    reiniciei o site para aplicar a responsividade.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
-    $("#carousel-images").slick({
-        autoplay: true,
-        arrows: false
-        
-    })
     
+    $('.carousel-images1').slick({
+        autoplay: true,
+        asNavFor: '.carousel-images2'
+    })
+
+    $('.carousel-images2').slick({
+        autoplay: true,
+        arrows: false,
+        asNavFor: '.carousel-images1'
+    })
 })
+
